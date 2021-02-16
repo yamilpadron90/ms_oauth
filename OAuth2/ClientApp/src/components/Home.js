61673b9fe8c0992cc6b102a83a06eb47d351c8b1
yamil <yamil@earpsoft.com>
2021-02-16 20:19:01 +0100

    asd

--- OAuth2/ClientApp/src/components/Home.js
+++ OAuth2/ClientApp/src/components/Home.js
@@ -8,25 +8,35 @@ export class Home extends Component {
     constructor() {
         super()
         this.state = {
-            name: null
+            name: null,
+            visible: true
         }
+        this.cb = this.cb.bind(this)
+    }
+
+    cb() {
+        this.setState({
+            visible: false
+        })
     }
 
 
   render () {
       return (
           <div style={{ marginTop: 24 }}>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        
-            <a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code
+              {this.state.visible === true && <div>
+                  <h1>Hello, world!</h1>
+                  <p>Welcome to your new single-page application, built with:</p>
+
+                  <a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code
             <br />
-          <a href='https://facebook.github.io/react/'>React</a> for client-side code
+                  <a href='https://facebook.github.io/react/'>React</a> for client-side code
             <br />
-            <a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling
-        
+                  <a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling
+
         <p>To help you get started, we have also set up:</p>
-        <MicrosoftOauth />
+              </div>}
+              <MicrosoftOauth cb={this.cb} />
       </div>
     );
   }
--- OAuth2/ClientApp/src/components/oauth.js
+++ OAuth2/ClientApp/src/components/oauth.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import MicrosoftLogin from "react-microsoft-login";
 
 const YOUR_CLIENT_ID = 'ac59499a-4413-4f2e-9899-1ec6c7e4d097'
@@ -6,16 +6,38 @@ const YOUR_CLIENT_ID = 'ac59499a-4413-4f2e-9899-1ec6c7e4d097'
 export default (props) => {
 
     const [name, setName] = useState(null)
+    const [auth, setAuth] = useState(false)
+
+    const accept = () => {
+
+    }
+
+    const reject = () => {
+
+    }
+
+    useEffect(() => {
+        if (auth === true) {
+            if (props.cb)
+                props.cb()
+        }
+    }, [auth])
 
   const authHandler = (err, data) => {
       console.log(err, data);
       if (data) {
           setName(data.account.name + " " + data.account.userName)
+          setAuth(true)
       }
   };
 
     return (
         <React.Fragment>
             {name === null ? <MicrosoftLogin clientId={YOUR_CLIENT_ID} authCallback={authHandler} /> : name}
+            {auth === true && <React.Fragment>
+                <button onClick={accept}>Accept</button>
+                {' '}
+                <button onClick={reject}>Reject</button>
+            </React.Fragment>}
     </React.Fragment>);
 };
